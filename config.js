90503bacba9e0a9ac482e9e2d9c2da8467f70706
Kewbin <87068650+ccprices@users.noreply.github.com>
2023-04-16 13:23:27 +0200

    dsa

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 import dotenv from 'dotenv'
 dotenv.config();
 
-const { API_DATA_URL } = process.env;
+//const { API_DATA_URL } = process.env;
 
 /*module.exports = {
 	apiURL: API_DATA_URL
 }*/
 
-console.log(API_DATA_URL);
+//console.log(API_DATA_URL);
--- main2.js
+++ main2.js
@@ -1,11 +1,12 @@
 import config from './config.js';
 
-const { API_DATA_URL } = config;
+let x = process.env.API_DATA_URL
+console.log(x)
 
 $.ajax({
     'async': false,
     'global': true,
-    'url': API_DATA_URL,
+    'url': x,
     'dataType': "json",
     'success': function(data) {
         basic = data
